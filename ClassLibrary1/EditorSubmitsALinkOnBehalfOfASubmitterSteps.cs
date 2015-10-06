d777ea317f8db02d84ab0b87437a92b299ea6c83
Tomasz Pluskiewicz <tpluskiewicz@pgs-soft.com>
2015-10-06 18:27:40 +0200

    first test

---
+++ ClassLibrary1/EditorSubmitsALinkOnBehalfOfASubmitterSteps.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace ClassLibrary1
+{
+    [Binding]
+    public class EditorSubmitsALinkOnBehalfOfASubmitterSteps
+    {
+        private string linkUrl;
+        private string linkDescritpion;
+
+        [Given(@"an URL '(.*)'")]
+        public void GivenAnURL(string url)
+        {
+            linkUrl = url;
+        }
+        
+        [Given(@"a description '(.*)'")]
+        public void GivenADescription(string description)
+        {
+            linkDescritpion = description;
+        }
+        
+        [When(@"Editor '(.*)' adds a Link on behalf of Submitter '(.*)'")]
+        public void WhenEditorAddsALinkOnBehalfOfSubmitter(string editorId, string submitterId)
+        {
+            ScenarioContext.Current.Set(new LinkSubmitted());
+        }
+        
+        [Then(@"Link must have been submitted")]
+        public void ThenLinkMustHaveBeenSubmitted()
+        {
+            var actualOutput = ScenarioContext.Current.Get<LinkSubmitted>();
+
+            Assert.That(actualOutput, Is.Not.Null);
+        }
+    }
+
+    public class LinkSubmitted
+    {
+    }
+}
---
+++ ClassLibrary1/SubmittingLinkOnBehalf.feature
@@ -0,0 +1,9 @@
+﻿Feature: Editor submits a Link on behalf of a Submitter
+
+Scenario: Editor sumbits a Link
+Given an URL 'http://pgs-soft.com'
+  And a description 'this is a company I work for'
+ When Editor 'tpluskiewicz' adds a Link on behalf of Submitter 'wmalara'
+ Then Link must have been submitted 
+	#| SubmitterId | URL                 | Description                  |
+	#| wmalara     | http://pgs-soft.com | this is a company I work for |
