2ab0a72b5171087aedda79465d0869281622d4ea
Tomasz Pluskiewicz <tpluskiewicz@pgs-soft.com>
2015-11-15 10:16:10 +0100

    move CommandResult to separate assembly

---
+++ src/PGS.DDD.Application/CommandResult.cs
@@ -0,0 +1,30 @@
+﻿using PGS.DDD.Domain;
+
+namespace PGS.DDD.Application
+{
+    public class CommandResult
+    {
+        private CommandResult(bool isSuccess)
+        {
+            IsSuccess = isSuccess;
+            Events = new DomainEvent[0];
+        }
+
+        public bool IsSuccess { get; private set; }
+
+        public DomainEvent[] Events { get; private set; }
+
+        public static CommandResult Success(params DomainEvent[] domainEvents)
+        {
+            return new CommandResult(true)
+            {
+                Events = domainEvents
+            };
+        }
+
+        public static CommandResult Failure()
+        {
+            return new CommandResult(false);
+        }
+    }
+}
